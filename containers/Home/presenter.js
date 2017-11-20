b0f5a16197c2b2d3ee5f33164826c3714116c201
V-SHY <vshy.ho@taessina.com>
2017-11-21 01:19:51 +0800

    :children_crossing: Pop up alert to confirm removal of start sleep or end sleep timestamp

--- containers/Home/presenter.js
+++ containers/Home/presenter.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { ScrollView, Button, Text } from 'react-native';
+import { ScrollView, Button, Text, Alert } from 'react-native';
 import Moment from 'moment';
 import styles from './style';
 
@@ -24,6 +24,40 @@ class Home extends React.Component<Props> {
       .format('YYYY-MM-DD(ddd) hh:mm(A) Z');
   }
 
+  handleRemoveStartSleep = () => {
+    const { startSleepTimestamps } = this.props;
+    if (startSleepTimestamps.length) {
+      Alert.alert(
+        'Remove Start Sleep',
+        `${this.displayDateTime(startSleepTimestamps[startSleepTimestamps.length - 1])}`,
+        [
+          { text: 'OK', onPress: () => this.props.doRemoveStartSleep() },
+          { text: 'Cancel', onPress: () => {} },
+        ],
+        {
+          cancelable: true,
+        }
+      );
+    }
+  };
+
+  handleRemoveStopSleep = () => {
+    const { endSleepTimestamps } = this.props;
+    if (endSleepTimestamps.length) {
+      Alert.alert(
+        'Remove Start Sleep',
+        `${this.displayDateTime(endSleepTimestamps[endSleepTimestamps.length - 1])}`,
+        [
+          { text: 'OK', onPress: () => this.props.doRemoveEndSleep() },
+          { text: 'Cancel', onPress: () => {} },
+        ],
+        {
+          cancelable: true,
+        }
+      );
+    }
+  };
+
   render() {
     return (
       <ScrollView style={styles.container} keyboardShouldPersistTaps="always">
@@ -37,8 +71,8 @@ class Home extends React.Component<Props> {
         ))}
         <Button title="Start Sleep" onPress={() => this.props.doSaveStartSleep(Date.now())} />
         <Button title="End Sleep" onPress={() => this.props.doSaveEndSleep(Date.now())} />
-        <Button title="Remove Last Start Sleep" onPress={() => this.props.doRemoveStartSleep()} />
-        <Button title="Remove Last End Sleep" onPress={() => this.props.doRemoveEndSleep()} />
+        <Button title="Remove Last Start Sleep" onPress={this.handleRemoveStartSleep} />
+        <Button title="Remove Last End Sleep" onPress={this.handleRemoveStopSleep} />
       </ScrollView>
     );
   }
