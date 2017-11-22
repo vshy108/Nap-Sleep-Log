a105ca86ea1c1255b1038b2a10f329a8b8b78eec
V-SHY <vshy.ho@taessina.com>
2017-11-23 00:30:38 +0800

    ✨ Display total sleeping time
    Remove the specific extra string in calculateTimeDifference function

--- containers/Home/presenter.js
+++ containers/Home/presenter.js
@@ -63,15 +63,14 @@ class Home extends React.Component<Props> {
     const truncateMilliseconds = Math.floor(difference / 1000);
     let hours = Math.floor(truncateMilliseconds / 3600);
     let minutes = Math.floor((truncateMilliseconds % 3600) / 60);
-
     if (hours < 10) {
       hours = `0${hours}`;
     }
     if (minutes < 10) {
       minutes = `0${minutes}`;
     }
-    return `Last sleep duration
-    ${hours} hours ${minutes} minutes`;
+    // return the conversion result only to increase reusability
+    return `${hours} hours ${minutes} minutes`;
   }
 
   renderCheckTimestampLengthText() {
@@ -88,9 +87,28 @@ class Home extends React.Component<Props> {
     } else {
       const lastStart = startSleepTimestamps[startLength - 1];
       const lastEnd = endSleepTimestamps[endLength - 1];
-      displayText = this.calculateTimeDifference(lastEnd - lastStart);
+      displayText = `Last sleep duration
+        ${this.calculateTimeDifference(lastEnd - lastStart)}`;
     }
     return <Text>{displayText}</Text>;
+  }
+
+  renderTotalSleepingTimeText() {
+    const { startSleepTimestamps, endSleepTimestamps } = this.props;
+    const startLength = startSleepTimestamps.length;
+    const endLength = endSleepTimestamps.length;
+    if (startLength && endLength) {
+      let total = 0;
+      const effectiveStartLength = startLength > endLength ? startLength - 1 : startLength;
+      for (let i = 0; i < effectiveStartLength; i += 1) {
+        total += endSleepTimestamps[i] - startSleepTimestamps[i];
+      }
+      return (
+        <Text>{`Total sleeping time:
+        ${this.calculateTimeDifference(total)}`}</Text>
+      );
+    }
+    return null;
   }
 
   render() {
@@ -130,6 +148,7 @@ class Home extends React.Component<Props> {
           />,
         ]}
         {this.renderCheckTimestampLengthText()}
+        {this.renderTotalSleepingTimeText()}
       </ScrollView>
     );
   }
